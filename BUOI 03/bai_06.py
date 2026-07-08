362e3fb07a555a8a15e9fe3abea5984e23ab253b
Nguyễn Nguyên Khôi <nguyennguyenkhoi6400@gmail.com>
2026-07-08 13:56:19 +0700

    Delete BUOI 03 directory

--- BUOI 03/bai_06.py
+++
@@ -1,9 +0,0 @@
-def phan_tu_cuoi_cung(a):
-    max = a[0]
-    for i in range(len(a)):
-        if max <= a[i]:
-            max = a[i]
-    return max
-
-a = [4, 2, 7, 1, 3]
-print(phan_tu_cuoi_cung(a))
